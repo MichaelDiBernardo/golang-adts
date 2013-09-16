0030cb914095520fec4896a2a4525c8de4291470
Michael DiBernardo <mikedebo@gmail.com>
2013-09-16 11:03:41 -0400

    Checkpoint for rational impl with little abstraction.

--- rational.go
+++ rational.go
@@ -1,10 +1,37 @@
 package rational
 
+import "fmt"
+
 type Rational struct {
-	Numerator int
+	Numerator   int
 	Denominator int
 }
 
 func New(num int, denom int) *Rational {
-	return &Rational{num, denom}
+    reduced_denom := gcd(num, denom)
+	return &Rational{num / reduced_denom, denom / reduced_denom}
+}
+
+func (self *Rational) String() string {
+	return fmt.Sprintf("%d/%d", self.Numerator, self.Denominator)
+}
+
+func (self *Rational) Equal(other *Rational) bool {
+	return self.Numerator == other.Numerator && self.Denominator == other.Denominator
+}
+
+func (self *Rational) Add(other *Rational) *Rational {
+	new_numerator := self.Numerator*other.Denominator + self.Denominator*other.Numerator
+	new_denominator := self.Denominator * other.Denominator
+	return New(new_numerator, new_denominator)
+}
+
+func gcd(m int, n int) int {
+	if m == n {
+		return m
+	} else if m > n {
+		return gcd(m-n, n)
+	} else {
+		return gcd(m, n-m)
+	}
 }
--- rational_test.go
+++ rational_test.go
@@ -1,7 +1,7 @@
 package rational_test
 
 import (
-    "rational"
+	"rational"
 	"testing"
 )
 
@@ -22,5 +22,56 @@ func TestDodgeConstructor(t *testing.T) {
 	}
 	if rat.Denominator != 2 {
 		t.Errorf("Denominator not 2! %d", rat.Denominator)
+	}
+}
+
+func TestString(t *testing.T) {
+	rat := rational.New(1, 2)
+	actual := rat.String()
+	if actual != "1/2" {
+		t.Errorf("String of 1/2 was %s", actual)
+	}
+}
+
+func TestEqualityOfIrreducible(t *testing.T) {
+	same_1, same_2 := rational.New(1, 2), rational.New(1, 2)
+	diff := rational.New(1, 3)
+
+	if !same_1.Equal(same_2) {
+		t.Errorf("Two same values were not equal.")
+	}
+
+	if same_1.Equal(diff) {
+		t.Errorf("Two different values were equal.")
+	}
+}
+
+func TestAddWithoutReduce(t *testing.T) {
+	first, second := rational.New(1, 2), rational.New(1, 3)
+	expected := rational.New(5, 6)
+
+	sum := first.Add(second)
+	if !sum.Equal(expected) {
+		t.Errorf("Sum to irreducible fraction: E - %s, A - %s", expected.String(), sum.String())
+	}
+}
+
+func CreateReducibleFraction(t *testing.T) {
+    rat := rational.New(10, 18)
+    if rat.Numerator != 5 {
+        t.Errorf("Numerator did not reduce! E - 5, A - %d", rat.Numerator)
+    }
+    if rat.Denominator != 9 {
+        t.Errorf("Denominator did not reduce! E - 9, A - %d", rat.Denominator)
+    }
+}
+
+func TestAddWithReduce(t *testing.T) {
+	first, second := rational.New(1, 2), rational.New(1, 3)
+	expected := rational.New(5, 6)
+
+	sum := first.Add(second)
+	if !sum.Equal(expected) {
+		t.Errorf("Sum to irreducible fraction: E - %s, A - %s", expected.String(), sum.String())
 	}
 }
