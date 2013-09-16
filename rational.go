d8f80298a0fdbfa27098c8b4cd78fbb90ec16879
Michael DiBernardo <mikedebo@gmail.com>
2013-09-16 10:23:04 -0400

    Starting with naive implementation

--- rational.go
+++ rational.go
@@ -1,18 +1,10 @@
 package rational
 
 type Rational struct {
-	numerator   int
-	denominator int
+	Numerator int
+	Denominator int
 }
 
 func New(num int, denom int) *Rational {
 	return &Rational{num, denom}
-}
-
-func (self *Rational) Numerator() int {
-	return self.numerator
-}
-
-func (self *Rational) Denominator() int {
-	return self.denominator
 }
--- rational_test.go
+++ rational_test.go
@@ -1,15 +1,26 @@
-package rational
+package rational_test
 
 import (
+    "rational"
 	"testing"
 )
 
 func TestCreateRational(t *testing.T) {
-	rat := New(1, 2)
-	if rat.Numerator() != 1 {
-		t.Errorf("Numerator not 1! %d", rat.Numerator())
+	rat := rational.New(1, 2)
+	if rat.Numerator != 1 {
+		t.Errorf("Numerator not 1! %d", rat.Numerator)
 	}
-	if rat.Denominator() != 2 {
-		t.Errorf("Denominator not 2! %d", rat.Denominator())
+	if rat.Denominator != 2 {
+		t.Errorf("Denominator not 2! %d", rat.Denominator)
+	}
+}
+
+func TestDodgeConstructor(t *testing.T) {
+	rat := rational.Rational{1, 2}
+	if rat.Numerator != 1 {
+		t.Errorf("Numerator not 1! %d", rat.Numerator)
+	}
+	if rat.Denominator != 2 {
+		t.Errorf("Denominator not 2! %d", rat.Denominator)
 	}
 }
